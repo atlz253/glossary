f32bede33b64446837e090b4e20a4d96f48bd6e7
Fedor Alekseev <f.alexeeff2010@yandex.ru>
2025-10-18 14:19:20 +0300

    редактирование термина по gRPC

--- src/grpc/services/term.py
+++ src/grpc/services/term.py
@@ -1,9 +1,9 @@
 from src.grpc.compiled.term.v1 import DeleteResponse
 from ..compiled.term.v1 import TermServiceBase, ListResponse, Term
-from ...gateway.term import term_list, get_term, create_term, delete_term
+from ...gateway.term import term_list, get_term, create_term, delete_term, edit_term
 from ...gateway import GatewayException, ItemNotFoundException
 from grpclib import GRPCError, Status
-from ...term import TermPost
+from ...term import TermPost, Term as TermDomain
 
 
 class TermService(TermServiceBase):
@@ -29,5 +29,12 @@ class TermService(TermServiceBase):
         try:
             delete_term(id)
             return DeleteResponse(ok=True)
+        except ItemNotFoundException as e:
+            raise GRPCError(Status.NOT_FOUND, str(e))
+
+    async def update(self, id: int, name: str, definition: str) -> Term:
+        try:
+            t = edit_term(TermDomain(id=id, name=name, definition=definition))
+            return Term(id=t.id, name=t.name, definition=t.definition)
         except ItemNotFoundException as e:
             raise GRPCError(Status.NOT_FOUND, str(e))
